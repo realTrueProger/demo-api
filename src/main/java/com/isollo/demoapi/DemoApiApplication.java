f6213a08ba12d863de958e943eacfb8d3efae772
Vladimir Solovev <vsolovev@inno.tech>
2024-01-12 13:52:37 +0300

    add GET /customers

--- src/main/java/com/isollo/demoapi/DemoApiApplication.java
+++ src/main/java/com/isollo/demoapi/DemoApiApplication.java
@@ -5,17 +5,90 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class DemoApiApplication {
+	//db
+	private static ArrayList<Customer> customers;
+
+	static {
+		customers = new ArrayList<>();
+		customers.add(new Customer(1, "Vova", "vova@mail.ru"));
+		customers.add(new Customer(2, "Dima", "dima@mail.ru"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApiApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String getHello() {
-		return "Hello";
+	@GetMapping("/customers")
+	public List<Customer> getHello() {
+		return customers;
+	}
+
+	static class Customer {
+		private Integer id;
+		private String name;
+		private String email;
+
+		public Customer() {
+		}
+
+		public Customer(Integer id, String name, String email) {
+			this.id = id;
+			this.name = name;
+			this.email = email;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			Customer customer = (Customer) o;
+			return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id, name, email);
+		}
+
+		@Override
+		public String toString() {
+			return "Customer{" +
+					"id=" + id +
+					", name='" + name + '\'' +
+					", email='" + email + '\'' +
+					'}';
+		}
 	}
 
 }
