d66ac38e311a442f4a92a588d4271b98167f2008
Vladimir Solovev <vsolovev@inno.tech>
2024-01-13 12:08:20 +0300

    add JPA repository

---
+++ src/main/java/com/isollo/demoapi/customer/CustomerJPADataAccessService.java
@@ -0,0 +1,25 @@
+package com.isollo.demoapi.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CustomerJPADataAccessService implements ICustomerDao{
+    private final ICustomerRepository customerRepository;
+
+    public CustomerJPADataAccessService(ICustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> getCustomer(Integer id) {
+        return customerRepository.findById(id);
+    }
+}
