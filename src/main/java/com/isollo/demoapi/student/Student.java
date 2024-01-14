57f40bc109c022c267e147c2d4ba41640833177e
Vladimir Solovev <vsolovev@inno.tech>
2024-01-14 13:03:43 +0300

    add Student entity

---
+++ src/main/java/com/isollo/demoapi/student/Student.java
@@ -0,0 +1,107 @@
+package com.isollo.demoapi.student;
+
+import jakarta.persistence.*;
+
+@Entity(name = "Student")
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_seq",
+            sequenceName = "student_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_seq"
+    )
+    @Column(
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String firstName;
+
+    @Column(
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String lastName;
+
+    @Column(
+            nullable = false,
+            columnDefinition = "TEXT",
+            unique = true
+    )
+    private String email;
+
+    @Column(
+            nullable = false
+    )
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String firstName, String lastName, String email, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
