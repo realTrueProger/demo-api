d3dcc727c98b24deee53446d389c45106e877891
Vladimir Solovev <vsolovev@inno.tech>
2024-01-14 13:50:39 +0300

    add custom query - find by email

--- src/main/java/com/isollo/demoapi/student/StudentRepository.java
+++ src/main/java/com/isollo/demoapi/student/StudentRepository.java
@@ -2,5 +2,8 @@ package com.isollo.demoapi.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findStudentByEmail(String email);
 }
