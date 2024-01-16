c02abb5d12311e3b745aba00ae92ede5d5547e31
Vladimir Solovev <vsolovev@inno.tech>
2024-01-16 12:16:48 +0300

    edit Customer for lombok

--- src/main/java/com/isollo/demoapi/customer/Customer.java
+++ src/main/java/com/isollo/demoapi/customer/Customer.java
@@ -1,80 +1,42 @@
 package com.isollo.demoapi.customer;
 
 import jakarta.persistence.*;
-
-import java.util.Objects;
+import lombok.*;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Customer {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @SequenceGenerator(
+            name = "customer_seq",
+            sequenceName = "customer_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_seq"
+    )
+    @Column(
+            updatable = false
+    )
     private Integer id;
+
     @Column(
             nullable = false
     )
     private String name;
+
     @Column(
             nullable = false
     )
     private String email;
 
-    public Customer() {
-    }
-
     public Customer(String name, String email) {
         this.name = name;
         this.email = email;
-    }
-
-    public Customer(Integer id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email);
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
     }
 }
