d9d5d2d4f4f349184cd63680c3b86d9087104150
Vladimir Solovev <vsolovev@inno.tech>
2024-01-26 19:20:46 +0300

    add full student constructor

--- src/main/java/com/isollo/demoapi/student/Student.java
+++ src/main/java/com/isollo/demoapi/student/Student.java
@@ -70,6 +70,14 @@ public class Student {
         this.age = age;
     }
 
+    public Student(Long id, String firstName, String lastName, String email, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
     public void addBook(Book book) {
         if(!this.books.contains(book)) {
             this.books.add(book);
