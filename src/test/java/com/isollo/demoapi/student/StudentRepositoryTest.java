1e1ab6cdbe6ebbcc192b667bdf8887a5fe8663f1
Vladimir Solovev <vsolovev@inno.tech>
2024-01-29 22:04:59 +0300

    add clean db after each test

--- src/test/java/com/isollo/demoapi/student/StudentRepositoryTest.java
+++ src/test/java/com/isollo/demoapi/student/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.isollo.demoapi.student;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,6 +13,11 @@ import static org.assertj.core.api.Assertions.*;
 class StudentRepositoryTest {
     @Autowired
     private StudentRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
 
     @Test
     void itShouldFindStudentByEmailNative() {
